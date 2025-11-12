7676491da124a10837a7cc6cc91574e188b601cf
AS-Adil <abdussamadadil1@gmail.com>
2025-11-13 01:44:49 +0600

    chore(deploy): prepare Express server for Vercel deployment

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
-const express = require('express')
-const cors = require('cors');
-const app = express()
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 3000;
 
-// console.log(process.env.DB_USER);
+// console.log('----------------------------pass',process.env.DB_USER);
 // console.log(process.env.DB_PASS);
 
-app.use(cors())
-app.use(express.json())
-
-
-
-
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ova35yv.mongodb.net/?appName=Cluster0`;
 
@@ -25,120 +20,118 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
+    const db = client.db("model_db");
+    const modelCollection = db.collection("models");
+    const purchasedCollection = db.collection("purchased");
 
-    const db = client.db('model_db')
-    const modelCollection = db.collection('models')
-    const purchasedCollection = db.collection('purchased')
+    app.get("/", (req, res) => {
+      res.send("AIventory Server is running ");
+    });
 
-    // all models 
-    app.get('/models', async(req,res)=>{
-      const result =await modelCollection.find().toArray()
-      res.send(result)
-    })
+    // all models
+    app.get("/models", async (req, res) => {
+      const result = await modelCollection.find().toArray();
+      res.send(result);
+    });
 
-    app.get('/latest-models', async(req, res) =>{
+    app.get("/latest-models", async (req, res) => {
+      const result = await modelCollection
+        .find()
+        .sort({ createdAt: -1 })
+        .limit(6)
+        .toArray();
+      res.send(result);
+    });
 
-      const result = await modelCollection.find().sort({createdAt: -1}).limit(6).toArray()
-      res.send(result)
+    app.get("/models/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await modelCollection.findOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
 
-    })
-
-    app.get('/models/:id',async (req, res) =>{
-      const id = req.params.id
-      const result = await modelCollection.findOne({_id :new ObjectId(id)})
-      res.send(result)
-    })
-
-    app.post('/models', async (req, res) =>{
-
+    app.post("/models", async (req, res) => {
       const newModel = req.body;
-      const result =await modelCollection.insertOne(newModel)
-      res.send(result)
+      const result = await modelCollection.insertOne(newModel);
+      res.send(result);
+    });
 
-    })
-
-    app.delete('/models/:id', async(req, res)=>{
+    app.delete("/models/:id", async (req, res) => {
       const id = req.params.id;
-      const result = await modelCollection.deleteOne({_id: new ObjectId(id)})
-      res.send(result)
-    })
+      const result = await modelCollection.deleteOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
 
-    app.put('/models/:id', async(req, res) =>{
+    app.put("/models/:id", async (req, res) => {
       const id = req.params.id;
-      const TheModel = req.body
-      const query = {_id : new ObjectId(id)}
+      const TheModel = req.body;
+      const query = { _id: new ObjectId(id) };
       const updatedModel = {
-        $set :TheModel
-      }
-      const result = await modelCollection.updateOne(query, updatedModel)
-      res.send(result)
+        $set: TheModel,
+      };
+      const result = await modelCollection.updateOne(query, updatedModel);
+      res.send(result);
+    });
 
-    })
-
-    app.get('/models-of', async(req, res) =>{
+    app.get("/models-of", async (req, res) => {
       const email = req.query.email;
-      console.log(email);
-      const result =await modelCollection.find({createdBy:email}).toArray()
-      res.send(result)
+      // console.log(email);
+      const result = await modelCollection.find({ createdBy: email }).toArray();
+      res.send(result);
+    });
 
-    })
-
-    app.post('/purchased-models/:id', async (req, res) =>{
+    app.post("/purchased-models/:id", async (req, res) => {
       const model = req.body;
-      const id =req.params.id
-      const result =await purchasedCollection.insertOne(model)
-      res.send(result)
+      const id = req.params.id;
+      const result = await purchasedCollection.insertOne(model);
+      res.send(result);
 
-
-        //downloads counted 
-      const query = {_id: new ObjectId(id)}
+      //downloads counted
+      const query = { _id: new ObjectId(id) };
       const update = {
         $inc: {
-          purchased: 1
-        }
-      }
-      const purchaseCounted = await modelCollection.updateOne(query, update)
-      res.send({result, purchaseCounted})
+          purchased: 1,
+        },
+      };
+      const purchaseCounted = await modelCollection.updateOne(query, update);
+      res.send({ result, purchaseCounted });
+    });
 
-
-    })
-
-      app.get('/purchased-models', async(req, res) =>{
+    app.get("/purchased-models", async (req, res) => {
       const email = req.query.email;
-      console.log(email);
-      const result =await purchasedCollection.find({purchasedBy:email}).toArray()
-      res.send(result)
+      // console.log(email);
+      const result = await purchasedCollection
+        .find({ purchasedBy: email })
+        .toArray();
+      res.send(result);
+    });
 
-    })
+    app.get("/purchased-models/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await purchasedCollection.findOne({
+        _id: new ObjectId(id),
+      });
+      res.send(result);
+    });
 
-     app.get('/purchased-models/:id',async (req, res) =>{
-      const id = req.params.id
-      const result = await purchasedCollection.findOne({_id :new ObjectId(id)})
-      res.send(result)
-    })
+    app.get("/search", async (req, res) => {
+      const search_text = req.query.search;
+      const result = await modelCollection
+        .find({ name: { $regex: search_text, $options: "i" } })
+        .toArray();
+      res.send(result);
+    });
 
-      app.get("/search", async(req, res) => {
-      const search_text = req.query.search
-      const result = await modelCollection.find({name: {$regex: search_text, $options: "i"}}).toArray()
-      res.send(result)
-    })
-
-
-   
-
-
-
-    
-
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -146,18 +139,8 @@ async function run() {
 }
 run().catch(console.dir);
 
+// app.listen(port, () => {
+//   console.log(`The server is running on port ${port}`);
+// });
 
-
-
-
-
-
-
-
-
-
-
-
-app.listen(port, () => {
-  console.log(`The server is running on port ${port}`)
-})
+module.exports = app;
